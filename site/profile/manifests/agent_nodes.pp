254e1422716b7e0b6f275989748735ec41f3cc8d
majerusjl <44145662+majerusjl@users.noreply.github.com>
2021-02-08 13:57:15 -0600

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,13 @@ class profile::agent_nodes {
 	include dockeragent
 	dockeragent::node {'centos7-web.localnet.com':}
 	dockeragent::node {'centos7-db.localnet.com':}
+	host {'centos7-web.localnet.com':
+		ensure => present,
+		ip     => '172.18.0.2',
+	}
+	host {'centos7-db.localnet.com':
+		ensure => present,
+		ip     => '172.18.0.3',
+	}
+	
 }
